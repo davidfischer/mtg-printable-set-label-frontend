dd69089a6b260f459e628b8a2349fadf17903c9e
David Fischer <djfische@gmail.com>
2022-03-20 00:21:46 -0700

    Enable running on cloud services

--- config/settings/development.py
+++ config/settings/development.py
@@ -10,6 +10,7 @@ AUTH_PASSWORD_VALIDATORS = []
 # django-debug-toolbar
 # https://django-debug-toolbar.readthedocs.io
 # --------------------------------------------------------------------------
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
 INSTALLED_APPS += ["debug_toolbar"]
 DEBUG_TOOLBAR_CONFIG = {
--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 """
+from django.conf import settings
 from django.urls import include
 from django.urls import path
 
@@ -10,3 +11,10 @@ from django.urls import path
 urlpatterns = [
     path("", include("frontend.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
